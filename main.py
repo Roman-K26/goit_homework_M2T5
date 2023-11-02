60e7413afffd257e91dc1b714b6b168eb187b14d
Roman-K26 <romankuspys777@gmail.com>
2023-11-02 20:04:45 +0300

    Commit 2

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
+import sys
+from datetime import datetime, timedelta
+
+import httpx
+import asyncio
 import platform
 
-import aiohttp
-import asyncio
 
+class HttpError(Exception):
+    pass
 
-async def main():
+MAX_DAYS = 10
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5') as response:
-
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
-            print('Cookies: ', response.cookies)
-            print(response.ok)
-            result = await response.json()
+async def request(url: str):
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url)
+        if r.status_code == 200:
+            result = r.json()
             return result
+        else:
+            raise HttpError(f"Error status: {r.status_code} for {url}")
 
 
-if __name__ == "__main__":
+async def main(index_day):
+    if index_day > MAX_DAYS:
+        raise ValueError("Максимальна кількість днів - 10")
+    d = datetime.now() - timedelta(days=int(index_day))
+    shift = d.strftime("%d.%m.%Y")
+    try:
+        response = await request(f'https://api.privatbank.ua/p24api/exchange_rates?date={shift}')
+        return response
+    except HttpError as err:
+        print(err)
+        return None
+
+
+if __name__ == '__main__':
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
-    print(r)
+    try:
+        if len(sys.argv) > 1:
+            index_day = int(sys.argv[1])
+            r = asyncio.run(main(index_day))
+            print(r)
+        else:
+            print("Вкажіть кількість днів.")
+    except ValueError as err:
+        print(err)
